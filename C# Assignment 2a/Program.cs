14b313c5d07e9687ba4dfba8741cba144aef1b45
Arindam Gohain <sunnyraj24@gmail.com>
2017-09-21 22:12:18 +0530

    Initial Commit

---
+++ C# Assignment 2a/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace C__Assignment_2a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array obj = new Array();
+            obj.enterSize();
+            obj.showArray();
+        }
+    }
+
+    class Array
+    {
+        int[] arr;
+        int size;
+        public void enterSize()
+        {
+            Console.WriteLine("Please enter the size of your array:");
+            size = int.Parse(Console.ReadLine());
+            arr = new int[size];
+        }
+
+        public void showArray()
+        {
+            Console.WriteLine("Please enter the array values:");
+            for(int i=0; i<size; i++)
+            {
+               arr[i] = int.Parse(Console.ReadLine());
+               if(i==10)
+               {
+                   Console.WriteLine("You reached 10");
+               } 
+            }
+        }
+        
+    }
+}
---
+++ C# Assignmetn 2b/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace C__Assignmetn_2b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1, n3 = 0;
+            Console.WriteLine("Please enter the number upto which you need the Series:");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            for(int i=0; i<=num; i++)
+            {
+                n3 = n1+n2;
+                Console.WriteLine(n3);
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
